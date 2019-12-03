a14612be2eae4a523d8520f4615b6aedf97a0f77
Ana Gauna <amgauna@gmail.com>
2019-12-03 14:18:28 -0300

    Update config.php

--- config.php
+++ config.php
@@ -2,20 +2,20 @@
 
 /* Configuração do arquivo PHP.ini
 
-default_charset = "UTF-8"
+default_charset: "UTF-8"
 
-date.timezone = "America/Sao_Paulo"
+date.timezone: "America/Sao_Paulo"
 
-asp_tags = Off
-register_globals = Off 
-register_long_arrays = Off
+asp_tags: Off
+register_globals: Off 
+register_long_arrays: Off
 
-memory_limit = 512    /*  Memória Mínimo de 64MB e Máximo de 512MB.  */
+memory_limit: 512    /*  Memória Mínimo de 64MB e Máximo de 512MB.  */
 
-track_errors = yes
-display_errors = On
-log_errors = On
-error_reporting = E_WARNING
-error_reporting = E_ALL
+track_errors: yes
+display_errors: On
+log_errors: On
+error_reporting: E_WARNING
+error_reporting: E_ALL
 
 
