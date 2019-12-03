ac4f302633a658ae0247047c0d1c9ff148a076d7
Ana Gauna <amgauna@gmail.com>
2019-12-03 14:16:03 -0300

    Update home-mulhertech.php

--- home-mulhertech.php
+++ home-mulhertech.php
@@ -5,29 +5,28 @@
 <html http-equiv="Content-Type" content="text/html; charset=ISO-8859-1;">  <!--  ISO-8859-1  -->
 
 <head>
-
-        <!-- <meta charset="UTF-8">   <!--  HTML 5  -->
-		<meta http-equiv="Content-Language" content="pt-br">
-        <!-- <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">  <!--  HTML 4  --> 
-	    <meta http-equiv="content-type" content="text/html;charset=ISO-8859-1">  <!--  ISO-8859-1  -->
+        
+	<title> Home | MulherTech </title>
+		
+        <meta name="author" content="Ana Mercedes Gauna"/>
+	<meta name="date" content="12/08/2018"/>
+	<meta name="description" content=" &#10024; &#10024; &#10024; MulherTech Management &#10024; &#10024; &#10024;
+		                        Porque o lugar da mulher, nÃ£o Ã© na cozinha, Ã© trabalhando na internet.  ;-)"/>	
+			  
+	<meta name="keywords" content="MulherTech, Mulher Tech, mulhertech, mulher tech, RJ, Rio de Janeiro"> 
+  
+        <meta charset="UTF-8" language="pt-br">   <!-- Charset = UTF-8 ou ISO-8859-1  -->
 
         <meta name="tech" content="HTML, CSS, JavaScript, Bootstrarp, PHP, JQuery, Ajax, MySQL, SQL, Java">									   
-		<meta name="google-site-verification" content="mulhertech.com.br"/>
+		<meta name="google-site-verification" content="mulhertech.com.br">
         <basefont size="12" color="black">
 		
-		<!-- redirecionamento de página no dominio  (propriedades padrão) -->
-		<!--  <meta http-equiv="refresh" content="360,url(' ')">  -->
-        <base href="http://mulhertech.com.br" target="_sef"/>
-        <meta http-equiv="refresh" content="999"/>
+        <!--  redirecionamento de pÃ¡gina no dominio  (propriedades padrÃ£o) -->
+	<!--  <meta http-equiv="refresh" content="360,url(' ')">  -->
+        <!--  <base href="http://mulhertech.com.br" target="_sef">  -->
+        <!--  <meta http-equiv="refresh" content="999">  -->
 		
-        <title>Home | MulherTech</title>
-		<meta name="author" content="Ana Mercedes Gauna"/>
-		<meta name="date" content="12/08/2018"/>
-		<meta name="description" content=" &#10024; &#10024; &#10024; MulherTech Management &#10024; &#10024; &#10024;
-		            Porque o lugar da mulher, não é na cozinha, é trabalhando na internet.  ;-)"/>	
-			  
-		<meta name="keywords" content="MulherTech, Mulher Tech, mulhertech, mulher tech, RJ, Rio de Janeiro"> 
-  
+   
         <!--  Google AdSense / Site Mulhertech -->
         <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         <script>
@@ -36,122 +35,125 @@
           enable_page_level_ads: true
           });
         </script>
-		<!--  Fim Google AdSence -->
+	<!--  Fim Google AdSence -->
   
-  <!-- Inserindo um favicon.ico (260x260px ou 70x70px) no dominio amgauna.com.br   -->	
+        <!-- Inserindo um favicon.ico (260x260px ou 70x70px) no dominio amgauna.com.br   -->	
         <!-- <link rel="shortcut icon" href="*" type="image/x-icon">  -->		
         <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon"/> </link>
 		
-		<!-- Setting the Viewport / making responsive webpages  -->   <!-- uso com Mobile -->
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<!-- Setting the Viewport / making responsive webpages  -->   <!-- uso com Mobile -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /> 
-		<link rel="w3mobile" href="https://www.w3schools.com/w3css/4/w3mobile.css"> </link>
+	<link rel="w3mobile" href="https://www.w3schools.com/w3css/4/w3mobile.css"> </link>
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">	</link>	
 
         
-		<!-- BOOTSTRAP 3.3.7  / JavaScript / CSS  --> 
+	<!-- BOOTSTRAP 3.3.7  / JavaScript / CSS  --> 
         <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
-	          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <!-- Optional theme -->
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" 
-	          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> 
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"> 
         <!-- Latest compiled and minified JavaScript -->
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
-	    integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 		
-        <!--  JQUERY / AJAX -->
+        <!--  JQUERY / AJAX / JavaScript / Bootstrap -->
         <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script> 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <!--  Fim  JQUERY / AJAX / JavaScript / Bootstrap -->
+
+        <!--  Awesome Fontes Icone --> 
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-		<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
-        <!--  Fim  JQUERY / AJAX -->
-		
-
-		<!--  Configuração SEO Google AdSense / AdWords / Analytics / Tag Manager / Google Maps -->
-					       
+	<!--  Fim Fonte Awesome -->						       
 		
 		
-        <style> /* CSS */
-                @charset 'iso-8859-1';
-		</style>
+  <style> /* CSS */
 		
-		<style>  /* CSS */
-			    .body {font: 400 15px Lato, sans-serif; line-height: 1.8; color: #818181;}
-                 * { box-sizing: border-box; }				
-                div, blockquote, article { font-family: Georgia, Helvetica, Serif; font-size: 12px; }
-				h2 { font-size: 24px; text-transform: uppercase; color: #303030; font-weight: 600; margin-bottom: 30px;}
-                h4 { font-size: 19px; line-height: 1.375em; color: #303030; font-weight: 400; margin-bottom: 30px;}
-				#google-maps { height: 100% }
-		</style>		
-							
+        @charset 'UTF-8';  /* Charset = UTF-8 ou ISO-8859-1  */
+		
+	.body {font: 400 15px Lato, sans-serif; line-height: 1.8; color: #818181;}
+		
+        * { box-sizing: border-box; }	
+	  
+         div, blockquote, article { font-family: Georgia, Helvetica, Serif; font-size: 12px; }
+	  
+	 h2 { font-size: 24px; text-transform: uppercase; color: #303030; font-weight: 600; margin-bottom: 30px;}
+	  
+         h4 { font-size: 19px; line-height: 1.375em; color: #303030; font-weight: 400; margin-bottom: 30px;}
+				
+	 #google-maps { height: 100% }
+										
 			
-		 <style>  /*  CSS / Background Image Responsive */
-                img background { max-width: 100%; height: auto; 
-                background-image: url('../img/motion2018-dark-vermelho.jpg');
-                background-repeat: no-repeat;
-                background-size: cover;  
-	            background-color: darkblue;
-                border: 0px solid silver; }
-         </style>
+         /*  CSS / Background Image Responsive */
+         img background { max-width: 100%; height: auto; 
+         background-image: url('../img/motion2018-dark-vermelho.jpg');
+         background-repeat: no-repeat;
+         background-size: cover;  
+	 background-color: darkblue;
+         border: 0px solid silver; }
+   	
 		
-		
-		 <style>  /* CSS / Image Responsive */
-		       .thumbnail , img { padding: 15px; border: none; border-radius: 0; }
-               .thumbnail img { width: 100%; height: 100%; margin-bottom: 10px; }
-         </style>
-		 
-		
-         <style>
-               .jumbotron { 
-			   font-family: Montserrat; san-serif;
-               background-color: #f4511e; /* Orange */
-               color: #fff;
-               padding: 100px 25px;
-               }
-         </style>		 
+         /* CSS / Image Responsive */
+         .thumbnail , img { padding: 15px; border: none; border-radius: 0; }
+         .thumbnail img { width: 100%; height: 100%; margin-bottom: 10px; }
+         		 		
+          .jumbotron { font-family: Montserrat; san-serif;
+                        background-color: #f4511e; /* Orange */
+                        color: #fff;
+                        padding: 100px 25px; }
+         		 			
 				
-				
-	    <style>  /* CSS / Bootstrap / Menu HOME */ 	
-		         /* color #f4511e laranja abobora escuro */
-			     /* background-color: #9F00FF; */ /* cor violeta médio royal */
-			     /* background-color: #c71585; */ /* cor rosa médio royal */
-			     /* background-color: #005aff; */ /* cor azul royal */
+	   /* CSS / Bootstrap / Menu HOME */ 	
+           /* color #f4511e laranja abobora escuro */
+	   /* color: #9F00FF / cor violeta mÃ©dio royal */
+	   /* background-color: #c71585; */ /* cor rosa mÃ©dio royal */
+	   /* background-color: #005aff; */ /* cor azul royal */
                
-			     // Extra small devices (portrait phones, less than 576px)
-                 // No media query since this is the default in Bootstrap
+	    /* Extra small devices (portrait phones, less than 576px) */
+            /* No media query since this is the default in Bootstrap  */
 
-                 // Small devices (landscape phones, 576px and up)
-                    @media (min-width: 576px) and (max-width: 767.98px) { font-size: 12px; z-index: 9999; }
+            /* Small devices (landscape phones, 576px and up) */
+            @media screen (min-width: 576px) and (max-width: 767.98px) { font-size: 12px; }
 
-                 // Medium devices (tablets, 768px and up)
-                    @media (min-width: 768px) and (max-width: 991.98px) { font-size: 14px; z-index: 9999; }
+            /* Medium devices (tablets, 768px and up) */
+            @media screen (min-width: 768px) and (max-width: 991.98px) { font-size: 14px; }
 
-                 // Large devices (desktops, 992px and up)
-                    @media (min-width: 992px) and (max-width: 1199.98px) { font-size: 16px; z-index: 9999; }
+            /* Large devices (desktops, 992px and up) */
+            @media screen (min-width: 992px) and (max-width: 1199.98px) { font-size: 16px; }
 
-                 // Extra large devices (large desktops, 1200px and up)
-                    @media (min-width: 1200px) { font-size: 18px; z-index: 9999; }
-			   
-	           #mynavbar .navbar { font-family: Montserrat, sans-serif; align:center; text:bold;}
-		  	   #mynavbar .navbar { margin-bottom: 0;
-                         background-color: black;
-                         z-index: 9999;
-                         border: 0;
-                         font-size: 12px;
-                         line-height: 1.42857143;
-                         letter-spacing: 4px;
-                         border-radius: 0;
-                        }
-               #mynavbar .navbar li a, .navbar .navbar-brand { color: #fff; }
-               #mynavbar .navbar-nav li a:hover, .navbar-nav li.active a { color: #005aff; background-color: #fff;}
-               .navbar-default .navbar-toggle { border-color: transparent; color: #fff; }
-	    </style>
-		 
+            /* Extra large devices (large desktops, 1200px and up) */
+            @media screen (min-width: 1200px) { font-size: 18px; z-index: 9999; }
 		
-        <script>
+	    /* CSS MENU */
+	    #myNavbar .navbar { font-family: Montserrat, sans-serif; align:center; text:bold;}
+	  
+            #mynavbar .navbar { margin-bottom: 0;
+                                background-color: black;
+                                z-index: 100%;
+                                border: 0;
+                                font-size: 12px;
+                                line-height: 1.42857143;
+                                letter-spacing: 4px;
+                                border-radius: 0;
+                                }
+	  
+               #mynavbar .navbar li a, .navbar .navbar-brand { color: #ffffff; }
+	  
+               #mynavbar .navbar-nav li a:hover, .navbar-nav li.active a { color: #005aff; background-color: #ffffff;}
+	  
+               .navbar-default .navbar-toggle { border-color: transparent; color: #ffffff; }
+	      /*  Fim CSS MENU  */
+	      
+	      /* CSS Footer */ 
+              footer .glyphicon { font-size: 12px; margin-bottom: 20px; color: #f4511e; }
+              /* Fim CSS Forter */
+
+        </style>
+
+		
+        <script language="JavaScript">
+		/* JavaScript MENU */
                 $(document).ready(function(){
                 // Add smooth scrolling to all links in navbar + footer link
                 $(".navbar a, footer a[href='#mynavbar']").on('click', function(event) {
@@ -172,13 +174,8 @@
                 } // End if
                 });
                 })
-        </script>	   
-			
-			
-        <style> /* CSS / Footer */ 
-               footer .glyphicon { font-size: 12px; margin-bottom: 20px; color: #f4511e; }
-        </style>
-
+		/* Fim JavaScript MENU */	
+        </script>	          
 				
 </head>  <!-- fim do cabecalho -->
 		
@@ -186,38 +183,48 @@
 <main> 
 
 <body style="background:black; width:100%; height:100%;" charset="iso-8859-1;">
-
-<!--	
-<div class="col-md-12" style="background:darkred; padding:0px; width:100%; height:100%;">
-    <figure> <img src="../img/mulhertech-management.jpg" width="100%" height="auto" align="center"> </figure>
+ 
+	
+<!--
+<div class="section"> 
+<div class="container">	
+<div class="row">
+ -->
+	
+	
+<div class="col-md-12" style="background:darkred; padding:0px; width:100%; height:100%;"> 
+    <figure> <img src="img/mulhertech-management.jpg" width="100%" height="auto" align="center"> </figure>
 </div>
--->
 
 
 <div class="row" style="background:aliceblue; color:black; border:1px; margin:0px; padding:0px;">  <!-- coluna 1 -->
 
 <div class="col-md-2" style="font-size:16px; margin:0px; padding:10px;" align="center">
-     <!--  Contador de Visitantes  da Página  -->   <!-- /* color #9F00FF; */ /* cor violeta médio royal */  -->
-     <center> <b> <?php require('contadorvisitas.php'); ?> </b> </center>
+     <!--  Contador de Visitantes  da PÃ¡gina  -->   <!-- /* color #9F00FF; */ /* cor violeta mÃ©dio royal */  -->
+     <center> <b> <?php require_once('contadorvisitas.php'); ?> </b> </center>
 </div>
 	 
 <div class="col-md-7" style="font-size:12px; margin:0px; padding:10px;" align="center">	 
      <!--  Redes Sociais / Links / Bootstrap  --> 		 
-     <center> <?php require('social-media.php'); ?>  </center>	
+     <center> <?php require_once('social-media.php'); ?>  </center>	
  
 
 <!-- inicio do MENU HOME / Bootstrap --> 
 <div class="col-md-3" style="margin:0; padding:2px;" align="center"> 
 
 <nav class="nav nav-navbar navbar-top" role="navigation"> 
-	 <div class="button navbar-btn"  id="mynavbar" align="center">
+     <div class="button navbar-btn"  id="mynavbar" align="center">
           <a class="btn btn-default btn-sm" id="#home"      href="home-mulhertech.php" target="_self"><b>Home</b></a>   
           <a class="btn btn-default btn-sm" id="#sobre-mim" href="sobre-mim-mulhertech.php" target="_self"><b>Sobre Mim</b></a>
-		  <a class="btn btn-default btn-sm" id="#contato"   href="contato-mulhertech.php" target="_self"><b>Contato</b></a> 
-     <!-- <a class="btn btn-primary btn-sm" id="sign-up" href="#"><span class="glyphicon glyphicon-user" target="_self"></span><b> Sign-Up </b></a>
-          <a class="btn btn-primary btn-sm" id="login"   href="#"><span class="glyphicon glyphicon-log-in" target="_self"></span><b>Login </b></a>
-	   -->
-	 </div>
+          <a class="btn btn-default btn-sm" id="#contato"   href="contato-mulhertech.php" target="_self"><b>Contato</b></a> 
+	     
+          <!-- <a class="btn btn-primary btn-sm" id="sign-up" href="#">
+          <span class="glyphicon glyphicon-user" target="_self"></span><b> Sign-Up </b></a>  -->
+	     
+           <!--  <a class="btn btn-primary btn-sm" id="login"   href="#">
+           <span class="glyphicon glyphicon-log-in" target="_self"></span><b>Login </b></a> -->
+	     
+    </div>
 </nav> 
 </div>
 <!-- fim do MENU -->
@@ -226,29 +233,15 @@
 
 <div class="row">
 <div class="col-md-12" style="background:black; margin:0px; padding:0px;" width="100%" height="100%">	
-     <!--  Imagem de Capa da Página -->
+     <!--  Imagem de Capa da PÃ¡gina -->
      <p> </p>
      <center>
      <iframe width="95%" height="2150px" align="center" frameborder="1" style="border:0" allowfullscreen 
-        src="http://mulhertech.blogspot.com" alt="_self"> </iframe> 
+        src=" " alt="_self"> </iframe> 
      </center>		
      <p> </p>
 </div>
 </div> 
-
-
-<!--
-<div class="jumbotron text-center">
-    <h1>Company</h1> 
-    <p>We specialize in blablabla</p> 
-</div>
- --> 
-
-<!--
-<div class="section"> 
-<div class="container">	
-<div class="row">
- -->
 
  
 </body>
